4d748ea6525bd889b5a81bae87cc624b1626bd12
AmiliaM <amiliamac@gmail.com>
2019-02-06 16:16:12 -0500

    Asteroids and fuel mining

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,18 @@ struct Planet {
     remaining_resources: isize,
 }
 
+#[derive(Debug, Clone)]
+struct Asteroid {
+    remaining_resources: isize,
+}
+
+#[derive(Debug, Clone)]
+enum Object {
+    Planet(Planet),
+    Asteroid(Asteroid)
+}
+
+
 fn is_within(pos: &Coord, pos2: &Coord, range: isize) -> bool {
     let d = ((pos.0 - pos2.0).pow(2) + (pos.1 - pos2.1).pow(2) + (pos.2 - pos2.2).pow(2)) as f64;
     if d.sqrt() < range as f64 {
@@ -36,7 +48,9 @@ fn move_ship(ship: &mut Ship) {
 }
 
 fn random_pos(x: isize) -> Coord {
-    (rand::thread_rng().gen_range(-x, x), rand::thread_rng().gen_range(-x, x), rand::thread_rng().gen_range(-x, x))
+    (rand::thread_rng().gen_range(-x, x), 
+    rand::thread_rng().gen_range(-x, x), 
+    rand::thread_rng().gen_range(-x, x))
 }
 
 fn game_over(reason: &str) {
@@ -45,9 +59,38 @@ fn game_over(reason: &str) {
 }
 
 fn main() {
-    let mut planets: HashMap<Coord, Planet> = HashMap::new();
-    for _ in 0..60 {
-        planets.insert(random_pos(100), Planet { remaining_resources: 3 });
+    let mut objects: HashMap<Coord, Object> = HashMap::new();
+    for _ in 0..25 {
+        let mut b = true;
+        let r = random_pos(100);
+        for (c, _) in objects.clone() {
+            if is_within(&c, &r, 3) {
+                b = false;
+                break;
+            }
+        }
+        if b {
+            objects.insert(
+                r, 
+                Object::Planet(Planet {remaining_resources: 3})
+            );
+        }
+    }
+    for _ in 0..80 {
+        let mut b = true;
+        let r = random_pos(100);
+        for (c, _) in objects.clone() {
+            if is_within(&c, &r, 3) {
+                b = false;
+                break;
+            }
+        }
+        if b {
+            objects.insert(
+                r, 
+                Object::Asteroid(Asteroid {remaining_resources: 3})
+            );
+        }
     }
     let mut ship = Ship {
         view_distance: 30, 
@@ -58,6 +101,7 @@ fn main() {
         landed: false,
     };
     let mut input: String;
+    println!("{:?}", objects);
     loop {
         println!("");
         println!("Your location is {:?}", ship.pos);
@@ -67,9 +111,12 @@ fn main() {
         }
         println!("You have {} food", ship.food);
         println!("You have {} fuel", ship.fuel);
-        for (c, p) in planets.iter() {
+        for (c, o) in objects.iter() {
             if is_within(&c, &ship.pos, ship.view_distance) {
-                println!("You can see a planet at position {:?}", c);
+                match o {
+                    Object::Planet(_) => println!("You can see a planet at position {:?}", c),
+                    Object::Asteroid(_) => println!("You can see an asteroid at position {:?}", c),
+                }
             }
         }
         loop {
@@ -136,19 +183,19 @@ fn main() {
                     else {
                         println!("You are not landed!");
                     }
-                }
+                },
                 "land" => {
                     if ship.landed {
                         println!("You are already landed!");
                     }
-                    else if planets.contains_key(&ship.pos) {
+                    else if objects.contains_key(&ship.pos) {
                         if (ship.vel.0 == 0) & (ship.vel.1 == 0) & (ship.vel.2 == 0) {
                             if ship.fuel < 1 {
                                 println!("Not enough fuel!");
                             }
                             else {
                                 ship.landed = true;
-                                ship.fuel =- 1;
+                                ship.fuel -= 1;
                                 println!("You have landed");
                                 break;
                             }
@@ -160,24 +207,37 @@ fn main() {
                     else {
                         println!("You can not land here!");
                     }
-                }
+                },
                 "mine" => {
                     if ship.landed {
-                        if planets[&ship.pos].remaining_resources > 0 {
-                            planets.get_mut(&ship.pos).unwrap().remaining_resources -= 1;
-                            ship.food += 4;
-                            println!("There are {} resources remaining", planets[&ship.pos].remaining_resources);
-                            break;
-                        }
-                        else {
-                            println!("Nothing left to mine");
-                        }
+                        objects.entry(ship.pos).and_modify(|object| match object {
+                            Object::Planet(ref mut p) => {
+                                if p.remaining_resources > 0 {
+                                    p.remaining_resources -= 1;
+                                    ship.food += 4;
+                                    println!("There are {} resources remaining", p.remaining_resources);
+                                }
+                                else {
+                                    println!("Nothing left to mine");
+                                }
+                            },
+                            Object::Asteroid(ref mut a) => {
+                                if a.remaining_resources > 0 {
+                                    a.remaining_resources -= 1;
+                                    ship.fuel += 4;
+                                    println!("There are {} resources remaining", a.remaining_resources);
+                                }
+                                else {
+                                    println!("Nothing left to mine");
+                                }
+                            }
+                        });
+                        break;
                     }
                     else {
                         println!("You need to land first!");
                     }
-                    
-                }
+                },
                 "wait" => break,
                 _ => println!("invalid option"),
             }
